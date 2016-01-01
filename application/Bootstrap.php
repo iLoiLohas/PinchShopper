73ce6675afc7e6c4244a4aa9c0e62f29293306df
iLohas <iloilo_has@yahoo.co.jp>
2016-01-02 00:42:09 +0900

    Smarty導入完了

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,25 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    /**
+     *
+     * Bootstrap Smarty view
+     */
+	protected function _initView() {
+		$smartyConfig	= $this->getOption('smarty');
+		$view = new Ext_View_Smarty($smartyConfig);
+// 		$view = new Zend_View_Smarty(
+// 				$smartyConfig['template_dir'], array(
+//     	    'compile_dir' => $smartyConfig['compile_dir'],
+//         	'config_dir'  => $smartyConfig['config_dir'],
+//         	'cache_dir'   => $smartyConfig['cache_dir'],
+//     		)
+// 		);
+		$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+		$viewRenderer->setViewSuffix('tpl');
+		$viewRenderer->setView($view);
+		Zend_Registry::set('smarty', $smartyConfig);
+		return $view;
+	}
 }
 
---
+++ templates/index/index.tpl
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Smarty Test</title>
+</head>
+<body>
+    {$str}
+</body>
+</html>
