2bd84347217069189d313d9c837d949ddbee051e
iLohas <iloilo_has@yahoo.co.jp>
2016-01-05 13:22:18 +0900

    autoデプロイ追加

--- src/application/Bootstrap.php
+++ src/application/Bootstrap.php
@@ -9,13 +9,6 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	protected function _initView() {
 		$smartyConfig	= $this->getOption('smarty');
 		$view = new Ext_View_Smarty($smartyConfig);
-// 		$view = new Zend_View_Smarty(
-// 				$smartyConfig['template_dir'], array(
-//     	    'compile_dir' => $smartyConfig['compile_dir'],
-//         	'config_dir'  => $smartyConfig['config_dir'],
-//         	'cache_dir'   => $smartyConfig['cache_dir'],
-//     		)
-// 		);
 		$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
 		$viewRenderer->setViewSuffix('tpl');
 		$viewRenderer->setView($view);
